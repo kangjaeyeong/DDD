2fd443cd189e0250022c334dbb0a4af4350156e8
kangjaeyeong <kangjaeyeong@naver.com>
2022-02-20 14:58:29 +0900

    DDD First Commit

---
+++ DDDProject/src/main/java/com/example/ddd/DddProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddProjectApplication.class, args);
+	}
+
+}
